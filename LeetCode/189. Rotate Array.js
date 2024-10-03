e0bdf942d765faac32e20528c1d5767d106e60e9
Pedro Ivo Bueno Sartório <pedroivobu@gmail.com>
2024-10-02 22:26:33 -0300

    First attempt to rotate array, the easiest approach

---
+++ LeetCode/189. Rotate Array.js
@@ -0,0 +1,51 @@
+/*
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+ 
+
+Follow up:
+
+Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+*/
+
+// For k times
+// Create varuabke to receive nums last element while removing it
+// Add it in the begining with unshift operation  
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    for (var i = 0; i < k; i++) {
+        let lastElement = nums.pop();
+        nums.unshift(lastElement);
+    }
+    return nums
+};
